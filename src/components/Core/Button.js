1f772e74e44e451921ae62f0ee314a0850a8ede0
unknown <dev.kubicki@gmail.com>
2024-08-21 16:12:06 +0200

    button item now are center

--- src/components/Core/Button.js
+++ src/components/Core/Button.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Button({btnName, direction}) {
   return (
-    <a href={`${direction}`} className=" dark:text-gray-300 dark:hover:text-tahiti-light transition hover:text-tahiti-dark font-bold  dark:border-tahiti-dark no-underline">{btnName}</a>
+    <a href={`${direction}`} className="flex items-center dark:text-gray-300 dark:hover:text-tahiti-light transition hover:text-tahiti-dark font-bold  dark:border-tahiti-dark no-underline">{btnName}</a>
   )
 }
